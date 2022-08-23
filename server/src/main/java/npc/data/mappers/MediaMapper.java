2c4688c83b4150b10fb93ae93dcad0bf26c9fc03
Chantell <101847336+chantellcruz@users.noreply.github.com>
2022-08-23 11:05:09 -0500

    added contents to media mapper. importer mediamapper class to mediajdbctemplaterepository.

--- server/src/main/java/npc/data/mappers/MediaMapper.java
+++ server/src/main/java/npc/data/mappers/MediaMapper.java
@@ -1,4 +1,19 @@
 package npc.data.mappers;
 
-public class MediaMapper {
+import npc.models.Media;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MediaMapper implements RowMapper<Media> {
+
+    @Override
+    public Media mapRow(ResultSet resultSet, int i) throws SQLException {
+        Media media = new Media();
+        media.setMediaId(resultSet.getInt("id"));
+        media.setImage_url(resultSet.getString("image_url"));
+        media.setTrailer_url(resultSet.getString("trailer_url"));
+        return media;
+    }
 }
