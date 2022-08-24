45634b3f11b599a2665210a8bfd47dc7e81ff9d7
ppaul895 <ppaul895@gmail.com>
2022-08-24 11:57:27 -0400

    Implemented Platform and GamePlatform services

--- server/src/main/java/npc/domain/GamePlatformService.java
+++ server/src/main/java/npc/domain/GamePlatformService.java
@@ -1,5 +1,58 @@
 package npc.domain;
 
-public class GamePlatformService {
+import npc.data.GamePlatformRepository;
+import npc.models.GamePlatform;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class GamePlatformService {
+    private final GamePlatformRepository repository;
+
+    public GamePlatformService(GamePlatformRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<GamePlatform> findAll() {
+        return repository.findAll();
+    }
+
+    public GamePlatform findById(int gamePlatformId) {
+        return repository.findById(gamePlatformId);
+    }
+
+    public Result<GamePlatform> add(GamePlatform gamePlatform) {
+        Result<GamePlatform> result = validate(gamePlatform);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        if (gamePlatform.getGamePlatformId() != 0) {
+            result.addMessage("GamePlatform ID cannot be set for `add` operation.", ResultType.INVALID);
+            return result;
+        }
+        gamePlatform = repository.add(gamePlatform);
+        result.setPayload(gamePlatform);
+        return result;
+    }
+
+    private Result<GamePlatform> validate(GamePlatform gamePlatform) {
+        Result<GamePlatform> result = new Result<>();
+        if (gamePlatform == null) {
+            result.addMessage("Game Platform cannot be null.", ResultType.INVALID);
+            return result;
+        }
+        if (gamePlatform.getGameId() <= 0) {
+            result.addMessage("Game ID is required.", ResultType.INVALID);
+        }
+        if (gamePlatform.getPlatformId() <= 0) {
+            result.addMessage("Platform ID is required.", ResultType.INVALID);
+        }
+        for (GamePlatform gp : repository.findAll()) {
+            if (gp.getGameId() == gamePlatform.getGameId() && gp.getPlatformId() == gamePlatform.getPlatformId()) {
+                result.addMessage("Game Platform is a duplicate.", ResultType.INVALID);
+            }
+        }
+        return result;
+    }
 }
--- server/src/test/java/npc/domain/GamePlatformServiceTest.java
+++ server/src/test/java/npc/domain/GamePlatformServiceTest.java
@@ -1,5 +1,119 @@
 package npc.domain;
 
+import npc.data.GamePlatformRepository;
+import npc.models.GamePlatform;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class GamePlatformServiceTest {
 
+    @Autowired
+    GamePlatformService service;
+
+    @MockBean
+    GamePlatformRepository repository;
+
+    @Test
+    void shouldFindAll() {
+        List<GamePlatform> expected = makeGamePlatformList();
+        when(repository.findAll()).thenReturn(expected);
+        List<GamePlatform> actual = service.findAll();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindById() {
+        GamePlatform expected = makeGamePlatformList().get(1);
+        when(repository.findById(2)).thenReturn(expected);
+        GamePlatform actual = service.findById(2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotAdd() {
+        // null
+        Result<GamePlatform> result = service.add(null);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        // gameId not set
+        GamePlatform gamePlatform = new GamePlatform();
+        gamePlatform.setPlatformId(2);
+        result = service.add(gamePlatform);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        // platformId not set
+        gamePlatform.setGameId(2);
+        gamePlatform.setPlatformId(0);
+        result = service.add(gamePlatform);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        // duplicate
+        gamePlatform.setGameId(1);
+        gamePlatform.setPlatformId(6);
+        when(repository.findAll()).thenReturn(makeGamePlatformList());
+        result = service.add(gamePlatform);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        // id already set
+        gamePlatform.setGamePlatformId(6);
+        gamePlatform.setGameId(2);
+        gamePlatform.setPlatformId(2);
+        result = service.add(gamePlatform);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldAdd() {
+        GamePlatform expected = new GamePlatform();
+        expected.setGamePlatformId(6);
+        expected.setGameId(2);
+        expected.setPlatformId(2);
+
+        GamePlatform arg = new GamePlatform();
+        arg.setGameId(2);
+        arg.setPlatformId(2);
+
+        when(repository.add(arg)).thenReturn(expected);
+        Result<GamePlatform> result = service.add(arg);
+        assertEquals(ResultType.SUCCESS, result.getType());
+        assertEquals(expected, result.getPayload());
+    }
+
+    private List<GamePlatform> makeGamePlatformList() {
+        GamePlatform gp1 = new GamePlatform();
+        gp1.setGamePlatformId(1);
+        gp1.setGameId(1);
+        gp1.setPlatformId(4);
+
+        GamePlatform gp2 = new GamePlatform();
+        gp2.setGamePlatformId(2);
+        gp2.setGameId(1);
+        gp2.setPlatformId(1);
+
+        GamePlatform gp3 = new GamePlatform();
+        gp3.setGamePlatformId(3);
+        gp3.setGameId(1);
+        gp3.setPlatformId(2);
+
+        GamePlatform gp4 = new GamePlatform();
+        gp4.setGamePlatformId(4);
+        gp4.setGameId(1);
+        gp4.setPlatformId(6);
+
+        GamePlatform gp5 = new GamePlatform();
+        gp5.setGamePlatformId(5);
+        gp5.setGameId(1);
+        gp5.setPlatformId(7);
+
+        return Arrays.asList(gp1, gp2, gp3, gp4, gp5);
+    }
 }
