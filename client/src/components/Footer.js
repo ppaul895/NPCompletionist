be7be2aa92f9de74677d4f4eb5abb6bbc3285adf
shawch7 <100060285+shawch7@users.noreply.github.com>
2022-08-27 16:40:59 -0400

    footer updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,42 +1,100 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 
 function Copyright(props) {
-	return (
-	  <Typography variant="body2" color="text.secondary" align="center" {...props}>
-		{'Copyright © '}
-		<Link color="inherit" href="/">
-		  NPCompletionist
-		</Link>{' '}
-		{new Date().getFullYear()}
-		{'.'}
-	  </Typography>
-	);
-  }
+  return (
+    <Box align="center">
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="/">
+          NPCompletionist
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </Box>
+  );
+}
 
 export default function StickyFooter() {
   return (
 
-      <Box
-        component="footer"
-		align="center"
-        sx={{
-          py: 10,
-          px: 2,
-          mt: 'auto',
-		  backgroundColor: '#222222',
-        }}
-      >
-        <Container maxWidth="sm">
-          <Typography variant="body1">
-            This is a sticky footer.
-          </Typography>
-          <Copyright />
-        </Container>
-      </Box>
-	
+    <Box
+      component="footer"
+      // align="center"
+      px={{ xs: 3, sm: 10 }}
+      py={{ xs: 5, sm: 10 }}
+      sx={{
+        py: 10,
+        px: 2,
+        mt: 'auto',
+        backgroundColor: '#222222',
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Box borderBottom={1}>Social</Box>
+            <Box>
+              <Link href='/' color="inherit">
+                Twitch
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/' color="inherit">
+                Twitter
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/' color="inherit">
+                Instagram
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box borderBottom={1}>Navigation</Box>
+            <Box>
+              <Link href='/' color="inherit">
+                Home
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/contact-us' color="inherit">
+                Contact Us
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/' color="inherit">
+                Featured Games
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box borderBottom={1}>Account</Box>
+            <Box>
+              <Link href='/backlog' color="inherit">
+                Backlog
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/sign-up' color="inherit">
+                Sign Up
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/sign-in' color="inherit">
+                Sign In
+              </Link>
+            </Box>
+            <Copyright />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+
   );
 }
