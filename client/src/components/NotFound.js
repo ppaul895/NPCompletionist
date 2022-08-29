e7d28285f83c25cf3b250f7af9124d924702c477
Chantell <101847336+chantellcruz@users.noreply.github.com>
2022-08-29 16:17:24 -0500

    created 404 page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,7 +1,23 @@
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 function NotFound() {
-    return (
-      <h2>Not Found</h2>
-    );
-  }
-  
-  export default NotFound;
+  return (
+    <Container align="center" sx={{ mt: 5, mb: 15 }}>
+      <Box sx={{ mb: 3 }}>
+        <img src="/images/404.gif" alt="NPCompletionist" sx={{ my: 10 }}></img>
+      </Box>
+      <Button variant="contained" size="large" href="/" sx={{
+        backgroundColor: '#ffffff',
+        width: '250px',
+        height: '60px',
+        fontSize: '20px',
+      }}>
+        Continue?
+      </Button>
+    </Container>
+  );
+}
+
+export default NotFound;
